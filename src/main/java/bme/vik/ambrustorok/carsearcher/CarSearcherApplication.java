f0798015479919c58c647e08b9c9d9a925e8938d
Török Ambrus <ambrus.torok@zooshgroup.com>
2020-10-03 23:55:14 +0200

    Initial commit by spring initializr

---
+++ src/main/java/bme/vik/ambrustorok/carsearcher/CarSearcherApplication.java
@@ -0,0 +1,13 @@
+package bme.vik.ambrustorok.carsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bme/vik/ambrustorok/carsearcher/CarSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package bme.vik.ambrustorok.carsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
