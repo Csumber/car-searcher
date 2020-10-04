8b1648189e6b9670b27d0d5e865303298adf1779
Török Ambrus <ambrus.torok@zooshgroup.com>
2020-10-04 14:14:25 +0200

    Adding modules, to initialize project

---
+++ vehicle-service/src/main/java/bme/vik/ambrustorok/vehicleservice/VehicleController.java
@@ -0,0 +1,23 @@
+package bme.vik.ambrustorok.vehicleservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VehicleController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin!";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/guest")
+    public String guest() {
+        return "Hello Guest!";
+    }
+}
